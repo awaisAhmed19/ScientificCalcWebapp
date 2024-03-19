4c797721af68d583a82be06f4319deb640076069
awaisAhmed19 <awaisahmedoffi@gmail.com>
2024-03-19 16:08:42 +0530

    designing a mininum precdence index finder- Returns the index of smallest precedence of the operator

--- FrontEnd/src/Parser.mjs
+++ FrontEnd/src/Parser.mjs
@@ -1,61 +1,48 @@
 import Tokenizer from "./Tokenizer.mjs";
-import Node from "./Node.mjs";
+//import Node from "./Node.mjs";
 import ExpressionChecker from "./ExpressionChecker.mjs";
 import { PRECEDENCE ,OPERATORS,LEFT_ASSOCIATIVE } from "./Precedence.mjs";
+let tk=new Tokenizer()
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class LiteralNode{
+    constructor(value){
+        this.value-value;
+    }
+}
 
 class Parser{
     constructor(){
         this.tokens=[];
         this.index=0;
         this.nodeStack=[];
+        
     }
     parser(expression){
         let tk=new Tokenizer();
-        let ec=new ExpressionChecker();
-        this.tokens=this.tk.tokenize(expression);
-        for (let token in this.tokens){
-            if( ec.isNumber(this.tokens[token])||
-                ec.isVar(this.tokens[token])||
-                ec.isConst(this.tokens[token])){
-                    this.nodeStack.push(new Node(this.tokens[token]));                
-            }
-            else if(ec.isOperator(this.tokens[token])){
-                let op = new Node(this.tokens[token]);
-                while(this.nodeStack.length>0){
-                    let top=this.nodeStack[this.nodeStack.length-1];
-                    if(ec.isOperator(top.value)){
-                        if((LEFT_ASSOCIATIVE[op.value] && PRECEDENCE[op.value]<=PRECEDENCE[top.value])||
-                            (!LEFT_ASSOCIATIVE[op.value] && PRECEDENCE[op.value]<PRECEDENCE[top.value])){
-                                this.nodeStack.pop();
-                                op.left=top;
-                        }else{
-                            break;
-                        }
-                    }else{
-                        break;
-                    }
-                }
-
-            }
-            
-            else if(ec.isLeftParen(this.tokens[token])){
-                this.nodeStack.push(new Node(this.tokens[token]));
-            }
-
-            else if(ec.isRightParen(this.tokens[token])){
-                while(this.nodeStack.length>0){
-                    let top=this.nodeStack[this.nodeStack.length-1];
-                    if(ec.isLeftParen(top.value)){
-                        this.nodeStack.pop();
-                        break;
-                    }else{
-                        this.nodeStack.pop();
-                    }
-                }
+        this.tokens=tk.tokenize(expression);
+        let minOperator
+        
+    }
+    minPrecedence(Exp) {
+        let minPrecedence = Infinity;
+        let index;
+        for (let oper = 0; oper < Exp.length; oper++) {
+            const currentPrecedence = PRECEDENCE[Exp[oper]];
+            if (currentPrecedence <= minPrecedence && LEFT_ASSOCIATIVE[Exp[oper]]) {
+                minPrecedence = currentPrecedence;
+                index=oper
             }
         }
+        return index
     }
 }
 
 let pr=new Parser();
-pr.parser("1+11+111");
+console.log(pr.minPrecedence("1-20-34*22"));
