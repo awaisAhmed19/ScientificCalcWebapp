cc892e9b4f6c50c6767fabbd2ad548ef4e6e828e
Awais Ahmed <awaisahmedoffi@gmail.com>
2024-03-25 20:26:45 +0530

    added Log,log2,log10, changed the logic of trig func

--- FrontEnd/src/Math_Module/Math.mjs
+++ FrontEnd/src/Math_Module/Math.mjs
@@ -23,6 +23,15 @@ class math{
     _sqrt_(x){ return this._exp_(x,0.5); }
     _deg_(x){ return (180/PI)*x; }
     _rad_(x){ return (PI/180)*x; }
+    _abs_(x){
+        if(x<0){
+            return x*-1;
+        }
+        return x;
+    }
+    _cbrt_(x){
+        return this._exp_(x,0.3333);
+    }
 
     _div_(x,y){
         if(y==0){
@@ -33,23 +42,84 @@ class math{
         }
     }
 
-    _exp_(x,y){
-        y=parseInt(y);
-        let temp=0
-        if(y<35){
-            if (y==0){ return 1; }
-            else{
-                if (y%2==0){
-                    temp=this._exp_(x,y/2);
-                    return temp*temp;
-                }else{
-                    temp=this._exp_(x,(y-1)/2);
-                    return temp*temp*x;
-                }
-            }
+    _log2_(x,precision=0.00000000000001){
+        if(x<=0){
+            return undefined;
+        }
+        if(x==1){
+            return 0;
         }
 
+        let low=0, high=x,res=-1,mid=0;
+
+        while(low<=high){
+            mid=low+(high-low)/2;
+
+            if(this._abs_(this._exp_(2,mid)-x)<precision){
+                res=mid;
+                break;
+            }
+            else if(this._exp_(2,mid)<x){
+                low=mid+precision;
+                res=mid;
+            }else{
+                high=mid-precision;
+            }
+        }
+        return res; 
     }
+    _log10_(x,precision=0.00000000000001){
+        if(x<=0){
+            return undefined;
+        }
+        if(x==1){
+            return 0;
+        }
+
+        let low=0, high=x,res=-1,mid=0;
+
+        while(low<=high){
+            mid=low+(high-low)/2;
+
+            if(this._abs_(this._exp_(10,mid)-x)<precision){
+                res=mid;
+                break;
+            }
+            else if(this._exp_(10,mid)<x){
+                low=mid+precision;
+                res=mid;
+            }else{
+                high=mid-precision;
+            }
+        }
+        return res; 
+    }
+
+    _ln_(x){
+        return this._log2_(x)/1.44269504089;
+    }
+
+    _exp_(x, y) {
+        if (y === 0) {
+            return 1;
+        }else if (y === 1) {
+            return x;
+        }else if(y<1){
+            let term=1,res=0,terms=10;
+            for (let i=0;i<terms-1;i++){
+                res+=term;
+                term=term*(y*Math.log(x)/(i+1));
+            }
+            return this._round_(res,4);
+        }else if (y % 2 === 0) {
+            let temp = this._exp_(x, y / 2);
+            return temp * temp;
+        } else {
+            let temp = this._exp_(x, (y - 1) / 2);
+            return temp * temp * x;
+        }
+    }
+    
 
     _fact_(x){
         if(x==0){ return 1; }
@@ -59,32 +129,38 @@ class math{
     
     _sin_(x){
         let angle=this._rad_(x);
-        let res=0.0,sign=1;
+        let res=0.0;
         
         for(let i=0;i<10;i++){
-            let term= this._exp_(angle,(2*i+1))/this._fact_(2*i+1);
-            res+=sign*term;
-            sign*=-1;
+            let coeff= this._exp_(-1,i)/this._fact_(2*i+1);
+            let term=coeff*(this._exp_(x,2*i+1));
+            res+=term
+            
         }
         return res;
     }
     _cos_(x){
         let angle=this._rad_(x);
-        let res=0.0,sign=1;
+        let res=0.0;
         
         for(let i=0;i<10;i++){
-            let term= this._exp_(angle,(2*i))/this._fact_(2*i);
-            res+=sign*term;
-            sign*=-1;
+            let coeff= this._exp_(-1,i)/this._fact_(2*i);
+            let term=coeff*this._exp_(x,2*i);
+            res+=term;
+            
         }
         return res;
     }
     _tan_(x){
+        //need to work on the logical error in tangent 
         let angle=this._rad_(x);
-        let a,b=0;
-        a=(angle-90)/(angle+90);
-        b=this._sqrt_((1-a)*(1+a))/2;
-        return this._round_(b,10);
+        let res=0;
+        for (let i=0;i<10;i++){
+            let coeff=2*i+1;
+            let term=(this._exp_(-1,i))*(this._exp_(angle,coeff))/this._fact_(coeff);
+            res+=term;
+        }
+        return res
     }
 
      _round_(x, n = 0) {
@@ -119,28 +195,8 @@ class math{
     _truncate_(x){ return parseInt(x); }
 }
 
+
 let m=new math();
-console.log("Inbuilt:", Math.round(3.45678));
-console.log("_round_:", m._round_(3.45678));
-
-console.log("Inbuilt:", Math.round(3.45678 * 100) / 100);
-console.log("_round_:", m._round_(3.45678, 2));
-
-console.log("Inbuilt:", Math.round(3.45678 * 1000) / 1000);
-console.log("_round_:", m._round_(3.45678, 3));
-
-console.log("Inbuilt:", Math.round(3.45678 * 100000) / 100000);
-console.log("_round_:", m._round_(3.45678, 5));
-
-console.log("Inbuilt:", Math.round(3.789));
-console.log("_round_:", m._round_(3.789));
-
-console.log("Inbuilt:", Math.round(-5.6789));
-console.log("_round_:", m._round_(-5.6789));
-
-console.log("Inbuilt:", Math.round(-5.6789 * 100) / 100);
-console.log("_round_:", m._round_(-5.6789, 2));
-
-
-
-
+console.log(Math.sin(Math.PI/6), Math.cos(Math.PI/6), Math.tan(Math.PI/6));
+console.log(m._sin_(Math.PI/6), m._cos_(Math.PI/6),m._tan_(Math.PI/6));
+//console.log(m._round_(0.5773502691896257,10))
